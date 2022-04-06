3fbaf35eae6637b6100aa2d5ab3f3283165c560b
Ishan <ishans1478@gmail.com>
2022-04-07 04:04:39 +0530

    Added Remaining Functionality

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const healthBar1 = document.querySelector("#health1");
 const healthBar2 = document.querySelector("#health2");
 const menu = document.querySelector("#main-menu");
 const fireBtn = document.querySelector(".Fire");
+const P1score = document.querySelector("#P1-score");
+const P2score = document.querySelector("#P2-score");
+const roundMenu = document.querySelector("#round-over");
+const winnerTitle = document.querySelector("#winner-title");
+const overMenu = document.querySelector("#game-over");
+const gameWinner = document.querySelector("#game-winner");
 
 var player1 = 100;
 var player2 = 100;
@@ -18,6 +24,15 @@ var turn = "P1";
 mainMenu();
 
 function mainMenu() {
+  player1 = 100;
+  player2 = 100;
+  document.querySelector("#health1").value = 100;
+  document.querySelector("#health2").value = 100;
+  player1wins = 0;
+  player2wins = 0;
+  turn = "P1";
+  roundCounter = 1;
+  fireBtn.style.right = "0px";
   healthBar1.style.display = "none";
   healthBar2.style.display = "none";
   round.style.display = "none";
@@ -26,18 +41,32 @@ function mainMenu() {
   moves.style.display = "none";
   banner.style.display = "none";
   menu.style.display = "flex";
+  roundMenu.style.display = "none";
+  overMenu.style.display = "none";
 }
 
 function startGame() {
-  exitBtn.style.display = "block";
-  startBtn.style.display = "none";
-  moves.style.display = "flex";
-  banner.style.display = "block";
-  healthBar1.style.display = "block";
-  healthBar2.style.display = "block";
-  round.style.display = "block";
-  menu.style.display = "none";
-  round.innerHTML = "Round : " + roundCounter;
+  if (player1 > 0 && player2 > 0) {
+    exitBtn.style.display = "block";
+    startBtn.style.display = "none";
+    moves.style.display = "flex";
+    banner.style.display = "block";
+    healthBar1.style.display = "block";
+    healthBar2.style.display = "block";
+    round.style.display = "block";
+    menu.style.display = "none";
+    roundMenu.style.display = "none";
+    round.innerHTML = "Round : " + roundCounter;
+  }
+  else {
+    roundOver();
+  }
+  
+}
+
+function nextRound() {
+    roundCounter++;
+    startGame();
 }
 
 function shoot() {
@@ -58,4 +87,55 @@ function shoot() {
       break;
     }
   }
+  startGame();
+}
+
+function roundOver() {
+  if(player1 <= 0) {
+    player2wins++;
+    winnerTitle.innerHTML = "Player 2 has won the round";
+  }
+  else {
+      player1wins++;
+      winnerTitle.innerHTML = "Player 1 has won the round";
+  }
+  if(player1wins >= 3 || player2wins >= 3 ) {
+    gameOver();
+  }
+  player1 = 100;
+  player2 = 100;
+  document.querySelector("#health1").value = 100;
+  document.querySelector("#health2").value = 100;
+  P1score.innerHTML = "Player 1's Score : " + player1wins;
+  P2score.innerHTML = "Player 2's Score : " + player2wins;
+  turn = "P1";  
+  roundMenu.style.display = "block";
+  fireBtn.style.right = "0px";
+  healthBar1.style.display = "none";
+  healthBar2.style.display = "none";
+  round.style.display = "none";
+  startBtn.style.display = "block";
+  exitBtn.style.display = "none";
+  moves.style.display = "none";
+  banner.style.display = "none";
+  menu.style.display = "none";
+}
+
+function gameOver() {
+  if(player1wins > player2wins) {
+      gameWinner.innerHTML = "Player 1 is the winner!";
+  }
+  else {
+    gameWinner.innerHTML = "Player 2 is the winner!";
+  }  
+  overMenu.style.display = "block";
+  fireBtn.style.right = "0px";
+  healthBar1.style.display = "none";
+  healthBar2.style.display = "none";
+  round.style.display = "none";
+  startBtn.style.display = "block";
+  exitBtn.style.display = "none";
+  moves.style.display = "none";
+  banner.style.display = "none";
+  roundMenu.style.display = "none";
 }
