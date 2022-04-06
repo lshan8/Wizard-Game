02a671f17eb25c7efc7371b03ad57ecad82efcaa
Ishan <ishans1478@gmail.com>
2022-04-06 15:20:55 +0530

    functionality changes

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const banner = document.getElementById("gameTitle");
 const moves = document.getElementById("moves");
 const exitBtn = document.querySelector(".exit");
 const startBtn = document.querySelector(".start");
+const round = document.querySelector("#round");
+const healthBar1 = document.querySelector("#health1");
+const healthBar2 = document.querySelector("#health2");
+const menu = document.querySelector("#main-menu");
+
+exitGame();
 
 function exitGame() {
+    healthBar1.style.display = "none";
+    healthBar2.style.display = "none";
+    round.style.display = "none";
     startBtn.style.display = "block"; 
     exitBtn.style.display = "none";
     moves.style.display = "none";
@@ -16,4 +25,8 @@ function startGame() {
     startBtn.style.display = "none";
     moves.style.display = "flex";
     banner.style.display = "block";
+    healthBar1.style.display = "block";
+    healthBar2.style.display = "block";
+    round.style.display = "block";
+    menu.style.display = "none";
 }
